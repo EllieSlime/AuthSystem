ec2858bafb0dd85674e8500c29704d49643936f3
EllieSlime <your_email@example.competer.pletnyov@gmail.com>
2025-09-24 01:36:11 +0300

    Commit4

--- auth_system/core/urls.py
+++ auth_system/core/urls.py
@@ -11,6 +11,9 @@ urlpatterns = [
     path("error401/", views.error401, name="error401"),
     path("error403/", views.error403, name="error403"),
     path("login/", views.login, name="login"),
-    path("register/", views.register, name="register"),
     path("settings/", views.settings, name="settings"),
+    path("lobby/", views.lobby, name="lobby"),
+    path("lobby_add/", views.lobby_add, name="lobby_add"),
+    path("lobby_set/", views.lobby_set, name="lobby_set"),
+    path("lobby_cre/", views.lobby_cre, name="lobby_cre"),
 ]
--- auth_system/core/views.py
+++ auth_system/core/views.py
@@ -18,9 +18,15 @@ def error403(request):
     return render(request, "core/error403.html")
 def login(request):
     return render(request, "core/login.html")
-def register(request):
-    return render(request, "core/register.html")
 def settings(request):
     return render(request, "core/settings.html")
+def lobby(request):
+    return render(request, "core/lobby.html")
+def lobby_add(request):
+    return render(request, "core/lobby_add.html")
+def lobby_set(request):
+    return render(request, "core/lobby_set.html")
+def lobby_cre(request):
+    return render(request, "core/lobby_cre.html")
 
 
